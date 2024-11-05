5ee4bc41483eb5e6edc42fb46e25c421de5bd6f5
lesam19092 <danigpro1337@gmail.com>
2024-11-05 14:59:51 +0300

    внедрил абстракцию

--- src/main/java/org/example/registration/controller/AuthController.java
+++ src/main/java/org/example/registration/controller/AuthController.java
@@ -3,7 +3,7 @@ package org.example.registration.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.registration.model.dto.JwtRequest;
 import org.example.registration.model.dto.RegistrationUserDto;
-import org.example.registration.service.AuthService;
+import org.example.registration.service.auth.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
