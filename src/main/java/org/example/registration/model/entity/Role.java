810c2819d64e29f6ebecf1cc3a40bc62f7c6f725
lesam19092 <danigpro1337@gmail.com>
2024-11-05 19:48:49 +0300

    поменял название

--- src/main/java/org/example/registration/model/entity/Role.java
+++ src/main/java/org/example/registration/model/entity/Role.java
@@ -3,7 +3,7 @@ package org.example.registration.model.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    ROLE_ADMIN,
+    Role_ADMIN,
     Role_USER;
 
     @Override
