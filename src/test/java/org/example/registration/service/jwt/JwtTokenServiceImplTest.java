df9a06f4a5755a81de040df0905476228f60b79d
lesam19092 <danigpro1337@gmail.com>
2024-11-08 11:09:14 +0300

    пофиксил тесты

--- src/test/java/org/example/registration/service/jwt/JwtTokenServiceImplTest.java
+++ src/test/java/org/example/registration/service/jwt/JwtTokenServiceImplTest.java
@@ -1,11 +1,15 @@
 package org.example.registration.service.jwt;
 
+import org.example.registration.service.auth.AuthServiceImpl;
+import org.example.registration.service.token.TokenService;
+import org.example.registration.service.user.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -13,6 +17,18 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 class JwtTokenServiceImplTest {
+
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @InjectMocks
+    private AuthServiceImpl authService;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private TokenService tokenService;
 
 
     @Mock
