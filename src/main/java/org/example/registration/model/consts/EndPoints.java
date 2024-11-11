d3100300e4df38ff6d8ec0f46dbd7eb02c7fe4e0
lesam19092 <danigpro1337@gmail.com>
2024-11-11 22:51:00 +0300

    переделал на enum

--- src/main/java/org/example/registration/model/consts/EndPoints.java
+++ src/main/java/org/example/registration/model/consts/EndPoints.java
@@ -1,9 +1,9 @@
 package org.example.registration.model.consts;
 
-public class EndPoints {
+public interface EndPoints {
 
-    public static final String SECURED = "/secured";
-    public static final String INFO = "/info";
-    public static final String ADMIN = "/admin";
-    public static final String LOGOUT = "/logout";
+    String SECURED = "/secured";
+    String INFO = "/info";
+    String ADMIN = "/admin";
+    String LOGOUT = "/logout";
 }
