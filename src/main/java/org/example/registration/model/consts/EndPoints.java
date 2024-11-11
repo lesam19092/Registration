bb519a65f0dcc9f0dab7bc739867985724be7698
lesam19092 <danigpro1337@gmail.com>
2024-11-11 22:48:34 +0300

    вернул все обратно)

---
+++ src/main/java/org/example/registration/model/consts/EndPoints.java
@@ -0,0 +1,9 @@
+package org.example.registration.model.consts;
+
+public class EndPoints {
+
+    public static final String SECURED = "/secured";
+    public static final String INFO = "/info";
+    public static final String ADMIN = "/admin";
+    public static final String LOGOUT = "/logout";
+}
